88f3f08d3032ece39f2ea08cff875fc0a5e8625f
srijan poudel <076bct081.srijan@pcampus.edu.np>
2020-08-28 13:57:09 +0545

    Updates

---
+++ precache-manifest.c735a29281364431cace7c336f18c610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9de33fbdb496dccef54d7c852082423",
+    "url": "/routinemkr/index.html"
+  },
+  {
+    "revision": "c682682fe3ff97bae3f2",
+    "url": "/routinemkr/static/css/main.5be3074a.chunk.css"
+  },
+  {
+    "revision": "ad19d619b93c192a5b56",
+    "url": "/routinemkr/static/js/2.43269595.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/routinemkr/static/js/2.43269595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c682682fe3ff97bae3f2",
+    "url": "/routinemkr/static/js/main.2a954a9a.chunk.js"
+  },
+  {
+    "revision": "b3eed1585c0218d8cf5e",
+    "url": "/routinemkr/static/js/runtime-main.09a2f631.js"
+  }
+]);
